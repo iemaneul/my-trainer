7be71fcddd3abed2047bdb470b316b303a522ca5
manuelson11 <emaneulcosta@gmail.com>
2026-06-01 00:11:57 -0300

    ajustado o primeiro exercicio

--- static/js/app.js
+++ static/js/app.js
@@ -22,12 +22,45 @@ if (treinoButtons.length) {
   });
 }
 
+var exercicioBoxes = document.querySelectorAll('.exercicio-box[data-page]');
+if (exercicioBoxes.length) {
+  exercicioBoxes.forEach(function (box) {
+    box.style.cursor = 'pointer';
+    box.addEventListener('click', function () {
+      var page = box.getAttribute('data-page');
+      if (page) {
+        window.location.href = page;
+      }
+    });
+  });
+}
+
+var scrollLinks = document.querySelectorAll('a[data-scroll-target]');
+if (scrollLinks.length) {
+  scrollLinks.forEach(function (link) {
+    link.style.cursor = 'pointer';
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      var targetId = link.getAttribute('data-scroll-target');
+      var target = document.getElementById(targetId);
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    });
+  });
+}
+
 var backArrows = document.querySelectorAll('img.arrow-blue');
 if (backArrows.length) {
   backArrows.forEach(function (img) {
     img.style.cursor = 'pointer';
     img.addEventListener('click', function () {
-      window.location.href = '../../index.html';
+      var backPage = img.getAttribute('data-back-page');
+      if (backPage) {
+        window.location.href = backPage;
+      } else {
+        window.location.href = '../../index.html';
+      }
     });
   });
 }
