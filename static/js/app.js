5a1e62ef1d8ee4ee3bdb0f4971493ef406940fa6
manuelson11 <emaneulcosta@gmail.com>
2026-05-31 22:28:19 -0300

    começo do ajuste do treino a

--- static/js/app.js
+++ static/js/app.js
@@ -9,3 +9,25 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+
+var treinoButtons = document.querySelectorAll('main .treino-box button[data-page]');
+if (treinoButtons.length) {
+  treinoButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      var page = button.getAttribute('data-page');
+      if (page) {
+        window.location.href = page;
+      }
+    });
+  });
+}
+
+var backArrows = document.querySelectorAll('img.arrow-blue');
+if (backArrows.length) {
+  backArrows.forEach(function (img) {
+    img.style.cursor = 'pointer';
+    img.addEventListener('click', function () {
+      window.location.href = '/';
+    });
+  });
+}
